98ac0f17096f897b64499920e5b7247c5dc24805
@khushiverma18 <147837115+khushiverma18@users.noreply.github.com>
2023-11-30 12:38:31 +0530

    Add files via upload

---
+++ string_q.java
@@ -0,0 +1,14 @@
+class string_q{
+    public static void main(String[] args){
+        String s1="hy buddy";
+        String s2="hello buddy";
+        System.out.println(s1.toUpperCase());
+        if(s1==s2){
+            System.out.println("equal");
+        }
+            else{
+                System.out.println("not equal");
+            }
+
+        }
+    }
