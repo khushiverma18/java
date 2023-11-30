03e4494b0de2c488eb589c8506183954712753b8
@khushiverma18 <147837115+khushiverma18@users.noreply.github.com>
2023-11-30 19:45:22 +0530

    Add files via upload

---
+++ table_t.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class table_t{
+    public static void main(String[] args){
+        int num=4;
+       /*  Scanner sc= new Scanner(System.in);
+        System.out.println("enter the value");
+        int num=sc.nextInt();*/
+        for(int i=1;i<=10;i++){
+            int t=num*i;
+System.out.printf("%d * %d=%d\n",num,i,t);
+        }
+
+    }
+
+}
