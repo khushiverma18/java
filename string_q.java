e7f29e9fd6120fc4ff83556d9732dfd7207e6cb3
@khushiverma18 <147837115+khushiverma18@users.noreply.github.com>
2023-11-30 19:24:12 +0530

    Update string_q.java

--- string_q.java
+++ string_q.java
@@ -3,6 +3,13 @@ class string_q{
         String s1="hy buddy";
         String s2="hello buddy";
         System.out.println(s1.toUpperCase());
+         System.out.println(s1.toUpperCase());
+        System.out.println(s2.toLowerCase());
+        System.out.println(s2.endsWith(s2));
+        System.out.println(s2.length());
+        System.out.println(s2.replace('o','y'));
+        System.out.println(s1.trim());
+        System.out.println(s2.compareToIgnoreCase(s1));
         if(s1==s2){
             System.out.println("equal");
         }
