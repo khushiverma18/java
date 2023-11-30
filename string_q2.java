46d41bf73cfd37c074d84a07dc41418c0bd0f074
@khushiverma18 <147837115+khushiverma18@users.noreply.github.com>
2023-11-30 12:39:23 +0530

    Add files via upload

---
+++ string_q2.java
@@ -0,0 +1,16 @@
+class string_q2{
+    public static void main(String[] arg){
+        String s="123456.5445";
+        boolean numeric =true;
+        try{
+            double num=Double.parseDouble("S");
+        }
+        catch(NumberFormatException e){
+            numeric =false;
+        }
+        if(numeric)
+        System.out.println(s+"is number");
+        else
+        System.out.println(s+"is not");
+    }
+}
