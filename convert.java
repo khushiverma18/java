24abd627f0ec78e2d15f992759b0edb4e7541d08
@khushiverma18 <147837115+khushiverma18@users.noreply.github.com>
2023-12-12 21:29:16 +0530

    Add files via upload

---
+++ convert.java
@@ -0,0 +1,28 @@
+class main{
+    public static void main(String[] args){
+        String num="2344";
+        //convert string to int using valueof
+        int stc=Integer.valueOf(num);
+        System.out.println(stc);
+        // //convert string to double using valueof
+        double sr=Double.valueOf(num);
+        System.out.println(sr);
+
+
+        int a=54;
+        int b=34;
+        //convrt int into double using typecasting
+       double c=a;
+       double d=b;
+       System.out.println(c);
+
+
+        double p=87.0D;
+       //convert double into int using typcasting
+       int k=(int)p;
+       System.out.println(k);
+
+
+
+    }
+}
