6f393a450a26d2ca794edf9b2667e2621224d404
@khushiverma18 <147837115+khushiverma18@users.noreply.github.com>
2024-02-19 18:54:31 +0530

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+class if_else{
+    public static void main(String[] args) {
+        //check you pass or not
+       /* Scanner ss=new Scanner(System.in);
+        System.out.println("enter total marks:");
+        float no=ss.nextInt();
+        System.out.println("enter your english no:");
+        float no1=ss.nextFloat();
+        System.out.println("enter your hindi no:");
+        float no2=ss.nextFloat();
+        System.out.println("enter your maths no:");
+        float no3=ss.nextFloat();
+        float result=no1+no2+no3;
+        float finalresult =result*100/(no*3);
+        System.out.println(finalresult);
+        if(finalresult>=40 && no1>=33 && no2>=33 && no3>=33){
+            System.out.println("congrats");
+        }
+        else{
+            System.out.println("sad");
+        }
+
+        //calculate tax
+        Scanner sa=new Scanner(System.in);
+        System.out.println("enter your income:");
+        float tax =0;
+        float income=sa.nextFloat();
+        if(income<2.5){
+            tax=tax+0;
+        }
+        else if( income>2.5f && income<5f){
+            tax=tax+0.05f*(income-2.5f);
+        }
+        else if(income>5f && income<10f){
+          tax=tax+0.05f*(5.0f-2.5f);
+          tax=tax+0.2f*(income-2.5f);
+        }
+        else if(income>10f){
+            tax=tax+0.05f*(5.0f-2.5f);
+            tax=tax+0.2f*(10.0f-5.0f);
+            tax=tax+0.3f*(income-10.0f);
+          }
+          System.out.println("total tax paid by employe is:"+tax);*/
+
+
+         }
+
+    }
