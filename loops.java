8f62ba57ee7f1fe89c8fd8656fc0e5b89c59908c
@khushiverma18 <147837115+khushiverma18@users.noreply.github.com>
2024-02-21 20:18:02 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,25 @@
+class loops{
+    public static void main(String[] args) {
+        //print pattern
+        int k=4;
+        for(int i=0;i<=k;i++){
+            for(int j=0;j<=i;j++){
+           System.out.print("*");
+
+            }
+           System.out.print("\n");
+
+            }
+            //reverse
+            int k1=4;
+        for(int i=k1;i>0;i--){
+            for(int j=0;j<i;j++){
+           System.out.print("*");
+
+            }
+           System.out.print("\n");
+
+            }
+            //a
+        }
+}
