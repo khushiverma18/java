9e01622828ccdaa09d978a6cc81793848b9c13b4
@khushiverma18 <147837115+khushiverma18@users.noreply.github.com>
2024-02-22 23:03:04 +0530

    Add files via upload

---
+++ recursions.java
@@ -0,0 +1,50 @@
+//factorial
+/* 
+public class recursions {
+    static int factorial(int n){
+        if(n==0||n==1){
+            return 1;
+        }
+        else{
+            return n*factorial(n-1);
+        }
+    }
+        public static void main(String[] arg){
+System.out.println(factorial(8) );
+        }
+}
+
+//sum of n natural no
+class recursions{
+    static int sum(int n){
+      if(n==0){
+        return 0;
+      }
+      else{
+        return n+sum(n-1);
+      }
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(9));
+    }
+}
+
+//fibonic serise
+class recursions{
+    static int fib(int n){
+      if(n==1){
+        return 0;
+      }
+      else if(n==2){
+       return 1; 
+      }
+      else{
+        return fib(n-1)+fib(n-2);
+      }
+    }
+public static void main(String[] args) {
+    System.out.println(fib(8));
+}
+}*/
+
+
